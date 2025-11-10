e68a26d5dfff0682dc8801ee530d7974d01c9945
AQchance <1363616727@qq.com>
2025-11-10 21:24:57 +0800

    修改网址信息

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://AQchance.github.io/",
-	base: "/AQchance-blog",
+  site: "https://blog.anqian.me/"
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
