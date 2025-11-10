19e2ff2eae6e6a5ce1255c88ce279619429c570c
AQchance <1363616727@qq.com>
2025-11-10 16:28:33 +0800

    地址名改了一下

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://AQchance.github.io",
+  site: "https://AQchance.github.io/",
 	base: "/AQchance-blog",
 	trailingSlash: "always",
 	integrations: [
